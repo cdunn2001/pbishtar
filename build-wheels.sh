62e7035b4efcb0f3b1974c6417a0a50b55633bb7
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-06-13 08:17:02 -0700

    fresh

--- build-wheels.sh
+++
@@ -1,4 +0,0 @@
-python setup.py bdist_wheel
-
-#setup.py bdist_ext -DFOO bdist_wheel -d TARGET
-#pip wheel --global-option bdist_ext --global-option -DFOO wheel
--- install-wheels.sh
+++
@@ -1,5 +0,0 @@
-# python -m pip install ... # in case pip is not in $PATH
-PIP_INSTALL=python -m pip install -v ${FALCON_PIP_USER}
-WHEEL_DIR=install-pip
-
-${PIP_INSTALL} --use-wheel --no-index --find-links=${WHEEL_DIR} package_name
