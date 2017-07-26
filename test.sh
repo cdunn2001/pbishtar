db3476ee0f2ab61ae0558f8e082ff63b5e4f374c
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-07-26 14:03:54 -0700

    Fix ldd

--- test.sh
+++ test.sh
@@ -7,6 +7,8 @@ virtualenv --no-setuptools --python=/opt/python-2.7.9/bin/python ${VENV}
 source ${VENV}/bin/activate
 export LD_LIBRARY_PATH=${VENV}/lib:$LD_LIBRARY_PATH
 tar xvfz /scratch/cdunn/foo.tar.gz -C ${VENV}/
-which fc_ovlp_stats.py
-fc_ovlp_stats.py -h
-ldd -r ${VENV}/lib/libblasr.so
+which fc_ovlp_stats
+fc_ovlp_stats -h
+#which ldd # does not work from /mnt/software/g/gcc/4.9.2/libc-2.5/bin/ldd
+#ldd --help
+/usr/bin/ldd -r ${VENV}/lib/libblasr.so
