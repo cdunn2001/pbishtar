83eedf5cbad4c67ee1725deb01d61d0116a4f483
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-07-31 19:49:09 -0700

    Test python stuff too

--- test.sh
+++ test.sh
@@ -7,8 +7,13 @@ virtualenv --no-setuptools --python=/opt/python-2.7.9/bin/python ${VENV}
 source ${VENV}/bin/activate
 export LD_LIBRARY_PATH=${VENV}/lib:$LD_LIBRARY_PATH
 tar xvfz /scratch/cdunn/foo.tar.gz -C ${VENV}/
+
 which fc_ovlp_stats
 fc_ovlp_stats -h
 #which ldd # does not work from /mnt/software/g/gcc/4.9.2/libc-2.5/bin/ldd
 #ldd --help
 /usr/bin/ldd -r ${VENV}/lib/libblasr.so
+#module load gcc/4.9.2 # You might need to do this yourself.
+python -c 'import falcon_kit; print falcon_kit'
+python -c 'import ConsensusCore; print ConsensusCore'
+python -c 'import ConsensusCore2; print ConsensusCore2'
