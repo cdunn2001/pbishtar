53c7189afa0c072db142b1ad9116f3664cab0807
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-03-09 07:18:43 -0800

    Better env defaults

--- env.sh
+++ env.sh
@@ -1,18 +1,12 @@
 source /mnt/software/Modules/current/init/bash
 module purge
 module load git
-
 module load python/2.7.13-UCS${UCS}
-#module load gcc/4.9.2
-#module load ccache
-#module load cmake
-#module load boost
-#module load zlib # IMPORTANT: Do NOT load this!
-#module load hdf5-tools/1.8.16
 
 #set -vex
 
-PFHOME=/scratch/cdunn/fork
-PREFIX=${PREFIX:-/scratch/cdunn/fork/.git/LOCAL${UCS}}
+# You should set PFHOME and UCS yourself.
+PFHOME=${PFHOME:-/scratch/cdunn/fork}
+PREFIX=${PFHOME}/.git/LOCAL${UCS}
 PYTHONUSERBASE=${PREFIX}
 export PFHOME PREFIX PYTHONUSERBASE
